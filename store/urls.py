c2f8dcb06e1f317409b9a895710cb21f21b429e2
Towhidmuhiuddin <mdtawhidmuhiuddin2020@gmail.com>
2023-02-08 13:56:37 +0600

    3rd commit for proper working in registration,login and logout page

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from store import views
+from store.controller import authview
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('collections/',views.collections, name='collections'),
     path('collections/<str:slug>',views.collectionsview, name='collectionsview'),
     path('collections/<str:cate_slug>/<str:mov_slug>',views.moviesview, name='movieview'),
+
+    path('register/', authview.registerpage, name='register'),
+    path('login/', authview.loginpage, name='login'),
+    path('logout/', authview.logoutpage, name='logout'),
+    
 
 ]
